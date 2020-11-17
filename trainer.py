8c26614918ca27463bdf0b4eddff9dbc8bb43d8b
Brian Wu <wubrian@berkeley.edu>
2020-11-17 12:03:28 -0800

    trying to train clean models

--- trainer.py
+++ trainer.py
@@ -275,6 +275,7 @@ def display_image(input, target, output, i):
     image = input[i].numpy().transpose((1,2,0))
     plt.figure()
     plt.imshow(image)
+    plt.savefig('input_{}.jpg'.format(i))
     plt.show()
 
 def save_checkpoint(state, is_best, filename='checkpoint.pth.tar'):
