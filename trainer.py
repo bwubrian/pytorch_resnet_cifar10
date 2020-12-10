bb09a5621046a97f9869ebf49ee0c89f99f141d1
Brian Wu <wubrian@berkeley.edu>
2020-12-09 16:07:53 -0800

    run clean

--- trainer.py
+++ trainer.py
@@ -90,7 +90,8 @@ def main():
         if os.path.isfile(args.resume):
             print("=> loading checkpoint '{}'".format(args.resume))
             checkpoint = torch.load(args.resume)
-            args.start_epoch = checkpoint['epoch']
+            if not args.evaluate::
+                args.start_epoch = checkpoint['epoch']
             best_prec1 = checkpoint['best_prec1']
             model.load_state_dict(checkpoint['state_dict'])
             print("=> loaded checkpoint '{}' (epoch {})"
